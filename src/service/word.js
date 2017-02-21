c9961215db8104fc24a40b2033d22a984e13d7fc
joelin109 <joe.lin1009@gmail.com>
2017-02-21 12:18:08 +0800

    adjust response data format

--- src/service/word.js
+++ src/service/word.js
@@ -11,8 +11,8 @@ let findList = (req, res, next) => {
         pageNum = req.query.page ? parseInt(req.query.page) : 1,
         values = [];
 
-    let _sql = "SELECT id, wort, wort_sex, plural, zh,en, is_regel, is_recommend " +
-        "FROM content_dictionary_de ORDER BY wort " + conn.limit(pageSize, pageNum);
+    let _sql = "SELECT id, wort, wort_sex as WortSex, plural, zh,en, is_regel as isregel, is_recommend as isrecommend" +
+        " FROM content_dictionary_de ORDER BY wort " + conn.limit(pageSize, pageNum);
 
 
     conn.query(_sql, values)
